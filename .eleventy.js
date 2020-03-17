74ddb48c25368d027fa73568d0e2447cb5d275c3
Spencer Tweedy <spencer@spencertweedy.com>
2020-03-16 21:00:40 -0500

    switch to jekyll

--- .eleventy.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(eleventyConfig) {
-  // Pass through the static assets
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("social.png");
-};
---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem "jekyll", "~> 4.0.0"
+
+group :jekyll_plugins do
+  gem "jekyll-tidy"
+  gem "jekyll-feed", "~> 0.12"
+end
